167381ce53c9fb882e2d7c968a3c8573fe0b0d63
Ade <Ade@DESKTOP-3LBH2N8>
2023-10-17 19:03:39 +0300

    Add project files.

---
+++ Ispas_Adelina_Lab2/Data/Ispas_Adelina_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ispas_Adelina_Lab2.Models;
+
+namespace Ispas_Adelina_Lab2.Data
+{
+    public class Ispas_Adelina_Lab2Context : DbContext
+    {
+        public Ispas_Adelina_Lab2Context (DbContextOptions<Ispas_Adelina_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Ispas_Adelina_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Ispas_Adelina_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
